10e4fbbe77d2e70249d9d20c4a2b86d67c5b2556
Iyiola-am <adekunle3317@gmail.com>
2020-10-24 17:04:49 +0100

    updated store state reducer actions

--- lib/store/store_state_reducer_action.dart
+++ lib/store/store_state_reducer_action.dart
@@ -1,4 +1,7 @@
 /// The set of actions that can be applied on the store state data.
 enum StoreStateReducerAction {
-  addItem
+  addCartItem,
+  updateCartItem,
+  removeCartItem,
+  clearCartItem
 }
