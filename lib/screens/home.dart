8a99e769739226cceb96b42286333a351f10654d
Iyiola-am <adekunle3317@gmail.com>
2020-10-24 17:42:39 +0100

    Added FAB to homescreen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,18 +3,42 @@ import 'package:pos_cart_mobile_app/store/store.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 
 class HomeScreen extends StatelessWidget {
+  Widget _buildAppBar() {
+    return AppBar(
+      title: StoreConnector<StoreState, String>(
+        converter: (store) => store.state.appName,
+        builder: (context, appName) => Text(appName),
+      )
+    );
+  }
+
+  Widget _buildBody() {
+    return Center(child: Text('Hello world'));
+  }
+
+  Widget _buildFAB() {
+    return StoreConnector<StoreState, VoidCallback>(
+      converter: (store) {
+        return () {
+          store.dispatch(
+            StoreStateReducer(action: StoreStateReducerAction.addCartItem)
+          );
+        };
+      },
+      builder: (buildContext, callback) => FloatingActionButton(
+        tooltip: 'Add item',
+        child: Icon(Icons.add),
+        onPressed: () => callback(),
+      )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: StoreConnector<StoreState, String>(
-          converter: (store) => store.state.appName,
-          builder: (context, appName) => Text(appName),
-        )
-      ),
-      body: Center(
-        child: Text('Hello world')
-      ),
+      appBar: _buildAppBar(),
+      body: _buildBody(),
+      floatingActionButton: _buildFAB()
     );
   }
 }
