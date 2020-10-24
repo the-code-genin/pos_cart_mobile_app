cdae1f1fee9cb4ecf5e8bf663c5ad5f5a2276f7f
Iyiola-am <adekunle3317@gmail.com>
2020-10-24 14:40:27 +0100

    Refactored cart item model

--- lib/models/cart_item.dart
+++ lib/models/cart_item.dart
@@ -1,7 +1,16 @@
 class CartItem {
   String name;
-  int quantity;
-  double price;
+  int _quantity;
+  double _price;
 
-  CartItem({this.name, this.quantity, this.price});
+  CartItem({this.name = '', quantity = 0, price = 0.0}) {
+    this.quantity = quantity;
+    this.price = price;
+  }
+
+  int get quantity => _quantity;
+  set quantity(int value) => value >= 0 ? _quantity = value : _quantity = 0;
+
+  double get price => _price;
+  set price(double value) => value >= 0 ? _price = value : _price = 0;
 }
