9412da54abac66ba6039060cb016918067758369
Iyiola-am <adekunle3317@gmail.com>
2020-10-24 18:03:16 +0100

    Updated app bar

--- lib/components/app_bar.dart
+++ lib/components/app_bar.dart
@@ -5,8 +5,28 @@ import 'package:flutter_redux/flutter_redux.dart';
 class CustomAppBar extends AppBar {
   CustomAppBar({Key key}) : super(key: key);
 
+  @override
   final Widget title = StoreConnector<StoreState, String>(
     converter: (store) => store.state.appName,
     builder: (context, appName) => Text(appName),
   );
+
+  @override
+  final List<Widget> actions = [
+    StoreConnector<StoreState, VoidCallback>(
+      converter: (store) {
+        return () {
+          store.dispatch(StoreStateReducer(
+            action: StoreStateReducerAction.clearCartItem
+          ));
+        };
+      },
+      builder: (buildContext, callback) {
+        return GestureDetector(
+          onTap: () => callback(),
+          child: Text('Clear')
+        );
+      }
+    )
+  ];
 }
