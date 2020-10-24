0a6a8b86b614c506d11505bcb38cbc952ef49d74
Iyiola-am <adekunle3317@gmail.com>
2020-10-24 21:03:05 +0100

    Home screen fully implmented

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,19 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:pos_cart_mobile_app/store/store.dart';
-import 'package:flutter_redux/flutter_redux.dart';
 import 'package:pos_cart_mobile_app/components/components.dart';
 
 class HomeScreen extends StatelessWidget {
-  Widget _buildBody() {
-    return Center(child: Text('Hello world'));
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: CustomAppBar(),
-        body: _buildBody(),
-        floatingActionButton: FAB()
+        appBar: HomeAppBar(),
+        body: HomeBody(),
+        bottomSheet: HomeBottomSheet(),
       );
   }
 }
