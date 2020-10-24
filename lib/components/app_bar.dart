ee0712bcf25a38b3a1cff989c1c9c0e9e2011085
Iyiola-am <adekunle3317@gmail.com>
2020-10-24 17:50:13 +0100

    Updated app bar

--- lib/components/app_bar.dart
+++ lib/components/app_bar.dart
@@ -2,13 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:pos_cart_mobile_app/store/store.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 
-class CustomAppBar extends StatelessWidget {
-  Widget build(BuildContext buildContext) {
-    return AppBar(
-      title: StoreConnector<StoreState, String>(
-        converter: (store) => store.state.appName,
-        builder: (context, appName) => Text(appName),
-      )
-    );
-  }
+class CustomAppBar extends AppBar {
+  CustomAppBar({Key key}) : super(key: key);
+
+  final Widget title = StoreConnector<StoreState, String>(
+    converter: (store) => store.state.appName,
+    builder: (context, appName) => Text(appName),
+  );
 }
