ee8ebf4f92c4ffa5e79b21360fbf0fd262949eed
Iyiola-am <adekunle3317@gmail.com>
2020-10-25 14:38:43 +0100

    Add new button on home screen refactored

--- lib/components/home/add_new_button.dart
+++ lib/components/home/add_new_button.dart
@@ -1,30 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:pos_cart_mobile_app/store/store.dart';
-import 'package:flutter_redux/flutter_redux.dart';
 
 class HomeAddNewButton extends StatelessWidget {
   Widget build(BuildContext buildContext) {
-    return StoreConnector<StoreState, VoidCallback>(
-      converter: (store) {
-        return () {
-          store.dispatch(
-            StoreStateReducer(
-              action: StoreStateReducerAction.addCartItem,
-            ),
-          );
-        };
+    return RaisedButton(
+      onPressed: () {
+        Navigator.pushNamed(buildContext, '/add-new-cart-item');
       },
-      builder: (buildContext, callback) => RaisedButton(
-        onPressed: callback,
-        padding: EdgeInsets.all(10),
-        color: Theme.of(buildContext).primaryColor,
-        child: Text(
-          'Add Item',
-          style: TextStyle(
-            color: Theme.of(buildContext).accentColor,
-          ),
-          textScaleFactor: 1.5,
+      padding: EdgeInsets.all(10),
+      color: Theme.of(buildContext).primaryColor,
+      child: Text(
+        'Add Item',
+        style: TextStyle(
+          color: Theme.of(buildContext).accentColor,
         ),
+        textScaleFactor: 1.5,
       ),
     );
   }
